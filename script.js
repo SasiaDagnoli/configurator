3cda19dae827381fda617a6bba25a750ff366d53
linegommesen <linegommesen@MacBook-Pro-tilhrende-Line.local>
2021-10-12 13:04:50 +0200

    adding badges to jacket + selected div when clicked

--- script.js
+++ script.js
@@ -1,10 +1,67 @@
+"use strict";
+
+//The model of all badges
+const badges = {
+  alien: false,
+  egg: false,
+  rose: false,
+  starwars: false,
+  yoda: false,
+};
+
 document.addEventListener("DOMContentLoaded", start);
 
 async function start() {
   let response = await fetch("jacket-config-01.svg");
   let mySvgData = await response.text();
   document.querySelector(".image").innerHTML = mySvgData;
+
+  //click on badges
+
+  document.querySelectorAll(".badge").forEach((badge) => {
+    badge.addEventListener("click", clickBadge);
+  });
+
   init();
+}
+function clickBadge(event) {
+  const target = event.currentTarget;
+  const badge = target.dataset.badge;
+  console.log("current badge", badge);
+
+  if (badges[badge] === false) {
+    badges[badge] = true;
+  } else {
+    badges[badge] = false;
+  }
+
+  if (badges[badge]) {
+    console.log(`Badge ${badge} is turned on!`);
+    document.querySelector(`[data-badge=${badge}]`).classList.remove("hide");
+    document.querySelector("#selected").cloneNode(createBadgeElement(badge));
+  } else {
+    console.log(`${badge} is turned off!`);
+    document.querySelector(`[data-badge=${badge}]`).classList.add("hide");
+    removeElement(badge);
+  }
+}
+function createBadgeElement(badge) {
+  // const li = document.createElement("li");
+  // li.dataset.badge = badge;
+  const selected = document.querySelector("#selected");
+
+  const img = document.createElement("img");
+  img.id = `${badge}`;
+  img.src = `badges/small_${badge}.png`;
+
+  selected.append(img);
+
+  return selected;
+}
+function removeElement(badge) {
+  const element = document.getElementById(`${badge}`);
+  console.log(element);
+  element.remove();
 }
 
 function init() {
