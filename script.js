a7638977252cea5cf3a7e06aaa3c5a0e6213b161
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-10-14 14:51:25 +0200

    addig badges to local storage and creating link with them works

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 "use strict";
 
-//The model of all badges
-const badges = {
-  alien: false,
-  egg: false,
-  rose: false,
-  starwars: false,
-  yoda: false,
-};
-
 document.addEventListener("DOMContentLoaded", loadSvg);
 
 let customImg = {
-  collar: "#ffffff",
-  top: "#ffffff",
-  sleeveRight: "#ffffff",
-  sleeveLeft: "#ffffff",
-  rightSide: "#ffffff",
-  leftSide: "#ffffff",
-  rightSideTop: "#ffffff",
-  leftSideTop: "#ffffff",
-  pocket: "#ffffff",
+  collar: "white",
+  top: "white",
+  sleeveRight: "white",
+  sleeveLeft: "white",
+  rightSide: "white",
+  leftSide: "white",
+  rightSideTop: "white",
+  leftSideTop: "white",
+  pocket: "white",
+  badges: {
+    alien: false,
+    egg: false,
+    rose: false,
+    starwars: false,
+    yoda: false,
+  },
 };
+
 let currentColor = "";
 
 async function loadSvg() {
@@ -37,18 +36,91 @@ async function loadSvg() {
 
   init();
 }
+
+function init() {
+  const url = window.location.href;
+  if (localStorage.length != 0) {
+    // gets object settings from local storage
+    let img = JSON.parse(localStorage.getItem("userCreation"));
+    console.log("after parsing", img);
+    applySavedImage(img);
+  } else if (url.indexOf("jacket_settings") > -1) {
+    // get url
+    const urlParams = new URLSearchParams(window.location.search);
+    const jacketParams = urlParams.get("jacket_settings");
+    console.log("jacket params", jacketParams);
+    const jacketObj = JSON.parse(urlParams.get("jacket_settings"));
+    console.log("jacket obj", jacketObj);
+
+    applySavedImage(jacketObj);
+  } else {
+    console.log("empty storage");
+    // sets color to white at the beginning
+    document.querySelectorAll("g").forEach((g) => {
+      setColor(g, "white");
+    });
+    selectPart();
+  }
+  registerButtons();
+}
+function registerButtons() {
+  const saveBtn = document.querySelector(".save");
+  const resetBtn = document.querySelector(".reset");
+  const shareBtn = document.querySelector(".share");
+  saveBtn.addEventListener("click", saveImg);
+  resetBtn.addEventListener("click", resetImg);
+  shareBtn.addEventListener("click", shareImg);
+}
+function selectPart() {
+  // makes each element of the jacket clickable
+  document.querySelectorAll("g").forEach((g) => {
+    g.addEventListener("click", (event) => {
+      setColor(event.target, currentColor);
+      // it stores the color value in the object so it can be reused after reload
+      let property = event.target.parentElement.id;
+      if (property != "badges") {
+        customImg[property] = currentColor;
+      }
+
+      if (g.id === "top") {
+        setColor(event.target.parentElement, currentColor);
+      }
+    });
+  });
+
+  // getting color from the squares
+  document.querySelectorAll(".color").forEach((element, i) => {
+    element.addEventListener("click", (event) => {
+      currentColor = event.target.style.backgroundColor;
+
+      console.log(currentColor);
+      cursor.style.backgroundColor = currentColor;
+    });
+  });
+  // creating stoke on each element on hover and remove when mouse is out
+  document.querySelectorAll("g").forEach((g) => {
+    g.addEventListener("mouseover", createStroke);
+    g.addEventListener("mouseout", removeStroke);
+  });
+}
+
+function setColor(element, colorString) {
+  element.style.fill = colorString;
+  cursor.style.backgroundColor = "transparent";
+}
+
 function clickBadge(event) {
   const target = event.currentTarget;
   const badge = target.dataset.badge;
   console.log("current badge", badge);
 
-  if (badges[badge] === false) {
-    badges[badge] = true;
+  if (customImg.badges[badge] === false) {
+    customImg.badges[badge] = true;
   } else {
-    badges[badge] = false;
+    customImg.badges[badge] = false;
   }
 
-  if (badges[badge]) {
+  if (customImg.badges[badge]) {
     console.log(`Badge ${badge} is turned on!`);
     document.querySelector(`[data-badge=${badge}]`).classList.remove("hide");
     document.querySelector("#selected").cloneNode(createBadgeElement(badge));
@@ -56,7 +128,6 @@ function clickBadge(event) {
     console.log(`${badge} is turned off!`);
     document.querySelector(`[data-badge=${badge}]`).classList.add("hide");
     removeElement(badge);
-    // animateBack(badge);
   }
 }
 function createBadgeElement(badge) {
@@ -71,7 +142,9 @@ function createBadgeElement(badge) {
   selected.append(image);
 
   //FLIP animation
-  const firstFrame = document.querySelector(`.${badge}-badge`).getBoundingClientRect();
+  const firstFrame = document
+    .querySelector(`.${badge}-badge`)
+    .getBoundingClientRect();
   console.log("firstframe", firstFrame);
 
   const lastFrame = image.getBoundingClientRect();
@@ -104,125 +177,17 @@ function createBadgeElement(badge) {
 }
 function removeElement(badge) {
   const element = document.getElementById(`${badge}`);
-  console.log(element);
+  console.log("what is this??!", element);
   element.remove();
 }
-// function animateBack(badge) {
-//   const element = document.getElementById(`${badge}`);
-
-//   //FLIP animation
-//   const firstFrame = document.querySelector(`.${badge}-badge`).getBoundingClientRect();
-//   console.log("firstframe", firstFrame);
-
-//   const lastFrame = element.getBoundingClientRect();
-//   console.log("lastframe", lastFrame);
-
-//   //position calculation
-//   const deltaX = lastFrame.left + firstFrame.left;
-//   const deltaY = lastFrame.top + firstFrame.top;
-
-//   //scale calculation
-//   const deltaWidth = lastFrame.width * firstFrame.width;
-//   const deltaHeight = lastFrame.height * firstFrame.height;
-
-//   element.animate(
-//     [
-//       {
-//         transformOrigin: "top left",
-//         transform: `translate(${deltaX}px, ${deltaY}px)
-//         scale(${deltaWidth}, ${deltaHeight})`,
-//       },
-//       { transformOrigin: "top left", transform: "none" },
-//     ],
-//     {
-//       duration: 200,
-//       easing: "ease-in",
-//     }
-//   );
-// }
 
 let cursor = document.querySelector(".cursor");
 document.body.addEventListener("mousemove", function (e) {
   (cursor.style.left = e.clientX + "px"), (cursor.style.top = e.clientY + "px");
 });
 
-function init() {
-  const url = window.location.href;
-  if (localStorage.length != 0) {
-    // gets object settings from local storage
-    let img = JSON.parse(localStorage.getItem("userCreation"));
-    applySavedImage(img);
-  } else if (url.indexOf("jacket_settings") > -1) {
-    // decodes url
-
-    const urlParams = new URLSearchParams(window.location.search);
-    const jacketParams = urlParams.get("jacket_settings");
-    console.log(jacketParams);
-    const jacketObj = JSON.parse(decodeURI(jacketParams));
-    let img = {};
-    for (var key in jacketObj) {
-      img[key] = decodeURIComponent(jacketObj[key]);
-    }
-    console.log(img);
-    applySavedImage(img);
-  } else {
-    console.log("empty storage");
-    // sets color to white at the beginning
-    document.querySelectorAll("g").forEach((g) => {
-      setColor(g, "white");
-    });
-    selectPart();
-  }
-  registerButtons();
-}
-function registerButtons() {
-  const saveBtn = document.querySelector(".save");
-  const resetBtn = document.querySelector(".reset");
-  const shareBtn = document.querySelector(".share");
-  saveBtn.addEventListener("click", saveImg);
-  resetBtn.addEventListener("click", resetImg);
-  shareBtn.addEventListener("click", shareImg);
-}
-function selectPart() {
-  // makes each element of the jacket clickable
-  document.querySelectorAll("g").forEach((g) => {
-    g.addEventListener("click", (event) => {
-      setColor(event.target, currentColor);
-
-      // it stores the color value in the object so it can be reused after reload
-      let property = event.target.parentElement.id;
-
-      customImg[property] = currentColor;
-
-      if (g.id === "top") {
-        setColor(event.target.parentElement, currentColor);
-      }
-    });
-  });
-
-  // getting color from the squares
-  document.querySelectorAll(".color").forEach((element, i) => {
-    element.addEventListener("click", (event) => {
-      currentColor = event.target.style.backgroundColor;
-
-      console.log(currentColor);
-      cursor.style.backgroundColor = currentColor;
-    });
-  });
-  // creating stoke on each element on hover and remove when mouse is out
-  document.querySelectorAll("g").forEach((g) => {
-    g.addEventListener("mouseover", createStroke);
-    g.addEventListener("mouseout", removeStroke);
-  });
-}
-
-function setColor(element, colorString) {
-  element.style.fill = colorString;
-  cursor.style.backgroundColor = "transparent";
-}
 function createStroke(e) {
   e.target.parentElement.classList.add("stroke");
-  /*   e.target.parentElement.style.fill = currentColor; */
 }
 function removeStroke(e) {
   e.target.parentElement.classList.remove("stroke");
@@ -230,10 +195,14 @@ function removeStroke(e) {
 
 function saveImg() {
   // modal box animations
-  document.querySelector("#save_confirmation").removeEventListener("animationend", removeAni);
+  document
+    .querySelector("#save_confirmation")
+    .removeEventListener("animationend", removeAni);
   document.querySelector("#save_confirmation").classList.remove("hidden");
   document.querySelector("#save_confirmation").classList.add("fade");
-  document.querySelector("#save_confirmation .x").addEventListener("click", closeDialog);
+  document
+    .querySelector("#save_confirmation .x")
+    .addEventListener("click", closeDialog);
 
   // saves object settings in local storage
   localStorage.setItem("userCreation", JSON.stringify(customImg));
@@ -241,11 +210,20 @@ function saveImg() {
 function resetImg() {
   // resets settings to default dettings
 
-  currentColor = "#ffffff";
+  currentColor = "white";
+  console.log("reset", customImg);
   for (let property in customImg) {
-    customImg[property] = currentColor;
-  }
+    if (property != "badges") {
+      customImg[property] = currentColor;
+    } else {
+      for (let status in customImg.badges) {
+        customImg.badges[status] = false;
 
+        document.querySelector(`[data-badge=${status}]`).classList.add("hide");
+        document.querySelector("#selected").innerHTML = "";
+      }
+    }
+  }
   // resets image to default color
   document.querySelectorAll("g path").forEach((g) => {
     setColor(g, "white");
@@ -260,42 +238,56 @@ function resetImg() {
 }
 
 function shareImg() {
-  document.querySelector("#share_confirmation").removeEventListener("animationend", removeAni);
+  // modal boxes
+  document
+    .querySelector("#share_confirmation")
+    .removeEventListener("animationend", removeAni);
   document.querySelector("#share_confirmation").classList.remove("hidden");
   document.querySelector("#share_confirmation").classList.add("fade");
-  document.querySelector("#share_confirmation .x").addEventListener("click", closeDialog);
+  document
+    .querySelector("#share_confirmation .x")
+    .addEventListener("click", closeDialog);
 
-  // encodes object settings and creates an url which can be shared
-  const toSend = {};
-  for (var key in customImg) {
-    toSend[key] = encodeURIComponent(customImg[key]);
-  }
-  console.log("http://127.0.0.1:5500/index.html?jacket_settings=" + encodeURI(JSON.stringify(toSend)));
-  /*  document.querySelector("#share_confirmation p span").textContent =
-    "http://127.0.0.1:5500/index.html?jacket_settings=" +
-    encodeURI(JSON.stringify(toSend)); */
-  document.querySelector("#share_confirmation p").addEventListener("click", copyLink);
+  // create link
+
+  const myLink = `https://martaa.dk/jacket-configurator/index.html?jacket_settings=${JSON.stringify(
+    customImg
+  )}`;
+  console.log("my link", myLink);
+
+  document
+    .querySelector("#share_confirmation p")
+    .addEventListener("click", copyLink);
 
   function copyLink() {
-    let link = "http://127.0.0.1:5500/index.html?jacket_settings=" + encodeURI(JSON.stringify(toSend));
-    navigator.clipboard.writeText(link);
-    document.querySelector("#share_confirmation p").textContent = "Link has been copied!";
+    navigator.clipboard.writeText(myLink);
+    document.querySelector("#share_confirmation p").textContent =
+      "Link has been copied!";
   }
 }
 
 function closeDialog() {
-  document.querySelector("#save_confirmation .x").removeEventListener("click", closeDialog);
-  document.querySelector("#share_confirmation .x").removeEventListener("click", closeDialog);
+  document
+    .querySelector("#save_confirmation .x")
+    .removeEventListener("click", closeDialog);
+  document
+    .querySelector("#share_confirmation .x")
+    .removeEventListener("click", closeDialog);
 
   document.querySelector("#save_confirmation").classList.remove("fade");
   document.querySelector("#share_confirmation").classList.remove("fade");
 
   document.querySelector("#save_confirmation").classList.add("leave");
   document.querySelector("#share_confirmation").classList.add("leave");
-  document.querySelector("#save_confirmation").addEventListener("animationend", removeAni);
+  document
+    .querySelector("#save_confirmation")
+    .addEventListener("animationend", removeAni);
 
-  document.querySelector("#share_confirmation").addEventListener("animationend", removeAni);
-  document.querySelector("#share_confirmation p").textContent = "Click to copy the link";
+  document
+    .querySelector("#share_confirmation")
+    .addEventListener("animationend", removeAni);
+  document.querySelector("#share_confirmation p").textContent =
+    "Click to copy the link";
 }
 function removeAni() {
   document.querySelector("#save_confirmation").classList.add("hidden");
@@ -311,10 +303,22 @@ function applySavedImage(img) {
   colorProperties.forEach((key) => {
     console.log("what is customimg key", customImg[key]);
     document.querySelectorAll("g").forEach((g) => {
-      if (g.id === key) {
+      if (g.id === key && key != "badges") {
         g.style.fill = customImg[key];
       }
     });
+
+    for (let status in customImg.badges) {
+      customImg.badges[status] = customImg.badges[status];
+      if (customImg.badges[status]) {
+        document
+          .querySelector(`[data-badge=${status}]`)
+          .classList.remove("hide");
+      } else {
+        document.querySelector(`[data-badge=${status}]`).classList.add("hide");
+      }
+    }
   });
+
   selectPart();
 }
